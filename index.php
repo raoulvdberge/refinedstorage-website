9badce2998e0c78ade90425de4b0800f4d76fe44
Raoul Van den Berge <raoulvdberge@gmail.com>
2016-11-13 17:18:03 +0100

    Wiki references.

--- index.php
+++ index.php
@@ -19,7 +19,9 @@ $capsule->addConnection([
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
 
-class Release extends Illuminate\Database\Eloquent\Model {
+class Release extends Illuminate\Database\Eloquent\Model {}
+class Wiki extends Illuminate\Database\Eloquent\Model {
+    protected $table = 'wiki';
 }
 
 $app = new \Slim\App;
@@ -34,8 +36,9 @@ $container['view'] = function ($container) {
 
     $function = new Twig_SimpleFunction('uri', function () {
     	return $_SERVER['REQUEST_URI'];
-	});
-	$view->getEnvironment()->addFunction($function);
+    });
+
+    $view->getEnvironment()->addFunction($function);
 
     return $view;
 };
@@ -51,10 +54,29 @@ $app->get('/releases', function (Request $request, Response $response) {
 
 $app->get('/releases/{id}', function (Request $request, Response $response, $args) {
 	$release = Release::find($args['id']);
-	if ($release == null) {
+	
+    if ($release == null) {
 		return $response->withStatus(404);
 	}
+
 	return $this->view->render($response, 'release.html', ['release' => $release]);
+});
+
+$app->get('/wiki/{url}', function(Request $request, Response $response, $args) {
+    $wiki = Wiki::where(['url' => $args['url']])->first();
+
+    if ($wiki == null) {
+        return $response->withStatus(404);
+    }
+
+    $wiki['body'] = preg_replace_callback("/\\[\\[.+?\\]\\]/", function ($match) {
+        $name = substr($match[0], 2, -2);
+        $reference = Wiki::where(['name' => $name])->first();
+
+        return '<a href="' . ($reference == null ? '#' : '/wiki/' . $reference['url']) . '" ' . ($reference == null ? 'style="color: #c00"' : '') . '">' . $name . '</a>';
+    }, $wiki['body']);
+
+    return $this->view->render($response, 'wiki.html', ['wiki' => $wiki]);
 });
 
 $app->run();
