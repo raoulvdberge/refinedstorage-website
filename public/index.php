1fd55cd822f1339ecab2c3cc09f80a22c2fb4dcb
raoulvdberge <raoulvdberge@gmail.com>
2017-04-02 15:42:39 +0200

    Add font for headers

--- public/index.php
+++ public/index.php
@@ -244,8 +244,8 @@ $container['view'] = function ($container) use ($roles) {
             if ($wiki == null) {
                 $data .= 'Unknown wiki page "' . $wikiPage . '"';
             } else if ($wiki->icon != null) {
-                $data .= '<div class="card pull-left" style="margin: 5px; margin-left: 1px;">';
-                $data .= '<div class="card-block" style="padding: 5px">';
+                $data .= '<div class="pull-left" style="margin: 5px; margin-left: 1px;">';
+                $data .= '<div class="" style="padding: 5px">';
                 $data .= '<a href="/wiki/' . $wiki->url . '"><img src="' . getIcon($wiki->icon) . '" class="wiki-icon-list" data-toggle="tooltip" data-placement="top" title="' . $wiki->name . '"></a>';
                 $data .= '</div>';
                 $data .= '</div>';
