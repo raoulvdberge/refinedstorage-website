5c8739f98297f8e7a637c7532fac8e2f2a75ee6f
raoulvdberge <raoulvdberge@gmail.com>
2018-04-12 22:25:30 +0200

    Add foreign key constraints to schema

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE releases
 (
   id         INTEGER NOT NULL
     PRIMARY KEY
-  autoincrement,
+  AUTOINCREMENT,
   version    TEXT    NOT NULL,
   changelog  TEXT    NOT NULL,
   url        TEXT    NOT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE users
 (
   id           INTEGER NOT NULL
     PRIMARY KEY
-  autoincrement,
+  AUTOINCREMENT,
   username     TEXT    NOT NULL,
   password     TEXT    NOT NULL,
   email        TEXT    NOT NULL,
@@ -26,14 +26,14 @@ CREATE TABLE users
 );
 
 ALTER TABLE releases
-  ADD CONSTRAINT releases_fk_users
-FOREIGN KEY (user_id) REFERENCES users;
+  ADD constraint releases_fk_users
+  FOREIGN KEY (user_id) REFERENCES users;
 
 CREATE TABLE wiki
 (
   id     INTEGER NOT NULL
     PRIMARY KEY
-  autoincrement,
+  AUTOINCREMENT,
   url    TEXT    NOT NULL,
   name   TEXT    NOT NULL,
   status INTEGER NOT NULL,
@@ -44,18 +44,18 @@ CREATE TABLE wiki_revisions
 (
   id            INTEGER NOT NULL
     PRIMARY KEY
-  autoincrement,
+  AUTOINCREMENT,
   wiki_id       INTEGER NOT NULL
-  CONSTRAINT wiki_revisions_fk_wiki
-  REFERENCES wiki,
+    CONSTRAINT wiki_revisions_fk_wiki
+    REFERENCES wiki,
   body          TEXT    NOT NULL,
   user_id       INTEGER NOT NULL
-  CONSTRAINT wiki_revisions_fk_user
-  REFERENCES users,
+    CONSTRAINT wiki_revisions_fk_user
+    REFERENCES users,
   date          INTEGER NOT NULL,
   reverted_by   INTEGER NOT NULL
-  CONSTRAINT wiki_revisions_fk_user_reverted_by
-  REFERENCES users,
+    CONSTRAINT wiki_revisions_fk_user_reverted_by
+    REFERENCES users,
   reverted_from INTEGER NOT NULL,
   hash          TEXT    NOT NULL
 );
